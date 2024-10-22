6fbf3d18dda6572f633c2cbec5422b08bddbcad2
marco <marcoaferreira@gmail.com>
2024-10-22 09:18:46 -0300

    [patch] removed callback, added suspend fun

--- app/src/main/java/com/devspace/myapplication/list/data/listService.kt
+++ app/src/main/java/com/devspace/myapplication/list/data/listService.kt
@@ -3,12 +3,13 @@ package com.devspace.myapplication.list.data
 import com.devspace.myapplication.common.model.RecipesResponse
 import com.devspace.myapplication.common.data.SearchRecipesResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface listService {
     @GET("recipes/random?number=20")
-    fun getRandom(): Call<RecipesResponse>
+    suspend fun getRandom(): Response<RecipesResponse>
 
     @GET("/recipes/complexSearch?")
     fun searchRecipes(@Query("query") query: String): Call<SearchRecipesResponse>
